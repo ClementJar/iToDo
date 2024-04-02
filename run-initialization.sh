19c3d42239c751f66cd8e7e00bec9c0104efdb7b
KUNDACL <kundacl@zra.org.zm>
2024-04-02 15:52:05 +0200

    added port

--- run-initialization.sh
+++ run-initialization.sh
@@ -3,4 +3,4 @@ sleep 90s
 
 # Run the setup script to create the DB and the schema in the DB
 # Note: make sure that your password matches what is in the Dockerfile
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P devOps2020 -d master -i dbo.sql
+/opt/mssql-tools/bin/sqlcmd -S 127.0.0.1,1433 -U sa -P devOps2020 -d master -i dbo.sql
